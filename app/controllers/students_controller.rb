9f09590f5d89cb516e45edbf80014427713f569d
Alex Mark <ajmark@andrew.cmu.edu>
2013-05-01 13:20:49 -0400

    messing around with nested forms, no luck

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
   
   def new
     @student = Student.new
+    @student.build_user
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Attraction was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
