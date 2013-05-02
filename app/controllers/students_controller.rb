2da4c9d1b565dd417725c16898edc3509d2a7f8c
Alex Mark <ajmark@andrew.cmu.edu>
2013-05-02 09:48:52 -0400

    Full functioning user form, no authentication

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,6 +18,11 @@ class StudentsController < ApplicationController
 
   def edit
     @student = Student.find(params[:id])
+    if @student.user.nil? 
+      @student.build_user
+    else 
+      user = @student.user
+    end 
   end
 
   def create
